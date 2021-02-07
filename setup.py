87806eddfa5d0952bc238cd51f4c3afdcee3723a
Edgar Teixeira <edgar.tx@outlook.com>
2021-02-07 19:08:50 -0300

    Fixed install_requires in setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
-import eul
+import dsul
 
 with open("README.md", "r", encoding="utf-8") as file:
     long_description = file.read()
 
 setup(
-    name="eul",
-    version=eul.__version__,
+    name="DSUL",
+    version=dsul.__version__,
     author="Edgar Teixeira",
     author_email="edgar.tx@outlook.com",
     description="Utility Library for Data Scientists",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/EdgarTeixeira/dsul",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -20,5 +20,10 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=[]
+    install_requires=[
+        "numpy",
+        "scipy",
+        "joblib",
+        "scikit-learn"
+    ]
 )
