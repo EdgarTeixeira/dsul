81a6cb134670d0d3fd8e695e86bbc6b64675eaa9
Edgar Teixeira <edgar.tx@outlook.com>
2021-02-07 18:51:53 -0300

    Rearrenged library

--- eul/confidence_intervals/computational.py
+++ eul/confidence_intervals/computational.py
@@ -1,86 +1,8 @@
-from enum import Enum
-from dataclasses import dataclass
-
 import numpy as np
 from joblib import Parallel, delayed
 from scipy import stats
 
-
-class BootstrapCI(Enum):
-    percentile = 0
-    normal = 1
-    bca = 2
-
-
-class IntervalType(Enum):
-    symmetric = 0
-    left = 1
-    right = 2
-
-
-@dataclass
-class ConfidenceInterval:
-    lower: float
-    upper: float
-    alpha: float
-
-    def contains(self, value: float) -> bool:
-        return self.lower <= value <= self.upper
-
-
-def mean_ci(sample, alpha, std_dev=None, interval_type=IntervalType.symmetric):
-    sample = np.asarray(sample)
-
-    if std_dev is None:
-        std_error = sample.std(ddof=1) / np.sqrt(sample.size)
-
-        df = sample.size - 1
-        dist = stats.t(df, loc=0, scale=1)
-
-    else:
-        std_error = std_dev / np.sqrt(sample.size)
-        dist = stats.norm(loc=0, scale=1)
-
-    lower_alpha, upper_alpha = _get_alphas(alpha, interval_type)
-    mean = sample.mean()
-    lower, upper = mean + std_error * dist.ppf([lower_alpha, upper_alpha])
-
-    return ConfidenceInterval(lower, upper, alpha)
-
-
-def proportion_ci(sample_or_proportion, alpha, interval_type=IntervalType.symmetric):
-    sample = np.asarray(sample_or_proportion)
-    p = sample.mean()
-    std_error = np.sqrt(p * (1 - p) / sample.size)
-
-    lower_alpha, upper_alpha = _get_alphas(alpha, interval_type)
-    dist = stats.norm(0, 1)
-    lower, upper = p + std_error * dist.ppf([lower_alpha, upper_alpha])
-
-    lower = max(lower, 0.0)
-    upper = min(upper, 1.0)
-
-    return ConfidenceInterval(lower, upper, alpha)
-
-
-def variance_ci(sample, alpha, interval_type=IntervalType.symmetric):
-    sample = np.asarray(sample)
-    sample_var = sample.var(ddof=1)
-    df = sample.size - 1
-
-    lower_alpha, upper_alpha = _get_alphas(alpha, interval_type)
-    dist = stats.chi2(df)
-    lower, upper = df * sample_var / dist.ppf([upper_alpha, lower_alpha])
-
-    return ConfidenceInterval(lower, upper, alpha)
-
-
-def stddev_ci(sample, alpha, interval_type=IntervalType.symmetric):
-    var_ci = variance_ci(sample, alpha, interval_type)
-
-    return ConfidenceInterval(var_ci.lower ** 0.5,
-                              var_ci.upper ** 0.5,
-                              alpha)
+from .base import BootstrapCI, ConfidenceInterval, IntervalType, _get_alphas
 
 
 class SimpleJackknife:
@@ -205,13 +127,3 @@ class SimpleBootstrap:
             raise NotImplementedError("Invalid Bootstrap CI method")
 
         return ConfidenceInterval(ci[0], ci[1], alpha)
-
-
-def _get_alphas(alpha, interval_type: IntervalType):
-    if interval_type == IntervalType.symmetric:
-        return alpha / 2, 1 - alpha / 2
-    elif interval_type == IntervalType.left:
-        return alpha, 1.0
-    elif interval_type == IntervalType.right:
-        return 0.0, 1 - alpha
-    raise NotImplementedError("Invalid IntervalType")
